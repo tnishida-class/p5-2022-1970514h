15190bb6eeede6d9a40a48e8881b56ca9d9849c4
1970514h <115205540+1970514h@users.noreply.github.com>
2022-10-13 13:01:16 +0900

    section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,7 +5,49 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+    
+    
+    if((i+j) % 2==1){
+      fill (196);
+    }
+    else{
+      fill(256);
+    }  
+    rect(i*8,j*8,8,8);
+   
+
+    } // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+  
+  }
+  for(let ll = 0; ll < 8; ll++){
+    for(let kk = 0; kk < 3; kk++){
+    
+    
+    if((ll+kk) % 2==1){
+      fill (255,0,0); 
+
+}
+else{
+  fill(255,255,255);
+}
+ellipse(ll*8+4,kk*8+4,7);
+    }
+  }
+
+  for(let ll = 0; ll < 8; ll++){
+    for(let kk = 5; kk < 8; kk++){
+    
+    
+    if((ll+kk) % 2==1){
+      fill (0,0,0); 
+
+}
+else{
+  fill(255,255,255);
+}
+ellipse(ll*8+4,kk*8+4,7);
     }
   }
 }
+  
+
