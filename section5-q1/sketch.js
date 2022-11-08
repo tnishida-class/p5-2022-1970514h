f5f745fb0bc2cfec6b3c99c27c99ff01cbf185cb
1970514h <115205540+1970514h@users.noreply.github.com>
2022-11-08 11:30:25 +0900

    section5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,19 +4,19 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46",20,20);
+  balloon("I love keyakizaka46",50,30,155,155,155);
   
 }
 
-function balloon(t,x,y){
+function balloon(t,x,y,cr,cg,cb){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
   
   
-  fill(0);
+  fill(cr,cg,cb); //三原色　cr=赤、cg=緑、cb=青
   rect(x, y, w + p * 2, h + p * 2); //p=padding ：吹き出しと文字の間の隙間
-  triangle(x,y,w+p*2/3,h+p*2,x+p+20,y+h+p+20);
+  triangle(x+10,y+h+p+p,x+30,y+h+p*2,x+p+20,y+h+p+20);
   fill(255);
   text(t, x+p, y+h + p);
  
